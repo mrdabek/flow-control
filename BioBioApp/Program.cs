ddd3ce6111dd1f40b14f83dd62620201c88773fa
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-05-26 13:39:03 +0200

    Refreshed tracked files using updated gitignore

--- BioBioApp/Program.cs
+++ BioBioApp/Program.cs
@@ -82,7 +82,7 @@ class Program
                             else
                             {
                                 totalPrice += 120;
-                            }
+                            }       
                         }
                     }
                     else
