45484a8eed07afa16575922945dd5e708e59c60f
mrdabek <81967634+mrdabek@users.noreply.github.com>
2026-06-09 21:57:49 +0200

    Add methods and moved logic to them

--- BioBioApp/Program.cs
+++ BioBioApp/Program.cs
@@ -30,73 +30,91 @@ class Program
                     break;
 
                 case "1":
-                    Console.WriteLine("Ange din ålder: ");
-                    if (int.TryParse(Console.ReadLine(), out int age))
-                    {
-                        if (age < 5 || age > 100)
-                        {
-                            Console.WriteLine("Fri entré");
-                        }
-                        else if(age < 20) 
-                        { 
-                            Console.WriteLine("Ungdomspris: 80kr"); 
-                        }
-                        else if (age > 64)
-                        { 
-                            Console.WriteLine("Pensionärspris: 90kr");
-                        }
-                        else
-                        {
-                            Console.WriteLine("Standardpris: 120kr");
-                        }
-                                            
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ogiltig input");
-                    }
+                    HandleSingleTicket();
                     break;
                     
                 
                 case "2":
-                    Console.WriteLine("Gruppbilljet: Hur många är ni?");
-                    if (int.TryParse(Console.ReadLine(), out int peopleAmount))
-                    {
-                        int totalPrice = 0;
-                        for (int i = 1; i <= peopleAmount; i++)
-                            Console.WriteLine($"Ange ålder för person {i}");
-                        if (int.TryParse(Console.ReadLine(), out int age2))
-                        {
-                            if (age2 < 5 || age2 > 100)
-                            {
-                                totalPrice += 0;
-                            }
-                            else if (age2 < 20)
-                            {
-                                totalPrice += 80;
-                            }
-                            else if (age2 > 64)
-                            {
-                                totalPrice += 90;
-                            }
-                            else
-                            {
-                                totalPrice += 120;
-                            }       
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ogiltig input");
-                    }
-                    
+                    HandleGroupTicket();
                     break;
-
-
+                
                 default:
                     Console.WriteLine("Felaktig input.");
                     break;
             }
         }    
     }
+
+    static void HandleSingleTicket()
+    {
+        Console.WriteLine("Ange din ålder: ");
+        if (int.TryParse(Console.ReadLine(), out int age))
+        {
+            if (age < 5 || age > 100)
+            {
+                // Console.WriteLine("Fri entré");
+            }
+            else switch (age)
+            {
+                case < 20:
+                    Console.WriteLine("Ungdomspris: 80kr");
+                    break;
+                case > 64:
+                    Console.WriteLine("Pensionärspris: 90kr");
+                    break;
+                default:
+                    Console.WriteLine("Standardpris: 120kr");
+                    break;
+            }
+                                            
+        }
+        else
+        {
+            Console.WriteLine("Ogiltig input");
+        }
+    }
+
+    static void HandleGroupTicket()
+    {
+        Console.WriteLine("Gruppbilljet: Hur många är ni?");
+        
+        if (int.TryParse(Console.ReadLine(), out int peopleAmount))
+        {
+            int totalPrice = 0;
+            for (int i = 1; i <= peopleAmount; i++)
+            {
+                Console.WriteLine($"Ange ålder för person {i}");
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    totalPrice += GetTicketPrice(age);
+                }
+                else 
+                { 
+                    Console.WriteLine("Ogiltig input"); 
+                } 
+            }
+            Console.WriteLine($"Antal personer: {peopleAmount}");
+            Console.WriteLine($"Totalt pris: {totalPrice} kr");
+        }
+        else
+        {
+            Console.WriteLine("Ogiltig input");
+        }
+        
+    }
+
+    static int GetTicketPrice(int age)
+    {
+        if (age < 5 || age > 100)
+            return 0;
+
+        if (age < 20)
+            return 80;
+        
+        if (age > 64)
+            return 90;
+        
+        return 120;
+    }
+    
 }
